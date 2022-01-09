7deb420e0b3757a63ffc1a1671e1962d31a94b02
Ryan Richardson <ryananthonyrichardson@gmail.com>
2022-01-08 23:36:23 -0800

    Make command line args more readable

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # busyboxx-downloader
-Hacky script to (slowly) download busybox content headlessly.
+
+Script for downloading BusyBoxx content headlessly. The BusyBoxx websites
+limit downloading to 5 items at a time while many of their libraries contain
+50, 100 or more assets.
+
+Relies on env variables `BUSYBOXX_EMAIL` and `BUSYBOXX_PASSWORD`.
+
+### Command line options
+
+**Required:**
+
+```
+   --source (STR) ('busy' or 'animation') - where to download from.
+```
+
+**Optional:**
+
+```
+   --library (INT) - target the `n`th of your owned libraries.
+   --start (INT) - skip downloading the libraries listed prior.
+   --speed (STR) - ('slow' or 'fast') time to wait between iterations.
+```
+
+**Example calls:**
+
+- `./download.rb --source busy`
+- `./download.rb --source busy --library 5`
+- `./download.rb --source animation --library 1 --start 3`
+- `./download.rb --source animation --library 1 --start 3  --speed fast`
--- download.rb
+++ download.rb
@@ -10,15 +10,35 @@ require 'byebug'
 ##                                                                        ##
 ## Relies on env variables BUSYBOXX_EMAIL and BUSYBOXX_PASSWORD           ##
 ##                                                                        ##
-## Example call:                                                          ##
-## ./download.rb - gets all content.                                      ##
-## ./download.rb 3 - gets all third lib content.                          ##
-## ./download.rb 5 4 - gets fifth lib content beginning less first three. ##
+## Command line options                                                   ##
+##   --source (STR) ('busy' or 'animation') - where to download from.     ##
+##   --library (INT) - target the `n`th of your owned libraries.          ##
+##   --start (INT) - skip downloading the libraries listed prior.         ##
+##   --speed (STR) - ('slow' or 'fast') time to wait between iterations.  ##
 ##                                                                        ##
+## Example calls:                                                         ##
+## ./download.rb --source busy                                            ##
+## ./download.rb --source busy --library 5                                ##
+## ./download.rb --source animation --library 1 --start 3                 ##
+## ./download.rb --source animation --library 1 --start 3  --speed fast   ##
 #############################################################################
 
+args = ARGV
+values = args.select{|item| !item.start_with? '--'}
+keys = (args - values).map{|item| item[2..-1] }
+
+options = {}
+keys.length.times do |i|
+  options[keys[i]] = values[i]
+end
+
+unless (values.count + keys.count) == args.count
+  raise 'Invalid argument(s). '\
+    'Valid arguments are --source, --library and --start'
+end
+
 SOURCE =
-  case ARGV[0]
+  case options['source']
   when 'animation'
     'Animation-Boxx'
   when 'busy'
@@ -27,10 +47,19 @@ SOURCE =
     raise 'Must specify source (busy or animation).'
   end
 
-BASE_URL = "https://www.#{SOURCE}.com"
+MINUTES =
+  case options['speed']
+  when 'fast'
+    5
+  when 'slow'
+    20
+  else
+    10
+  end
 
-LIB_NUMBER = ARGV[1].to_i || nil
-FIRST_ITEM = ARGV[2].to_i || nil
+BASE_URL = "https://www.#{SOURCE}.com"
+LIB_NUMBER = options['library'].to_i || nil
+START = options['start'].to_i || nil
 
 Watir.default_timeout = 60
 prefs = {
@@ -43,6 +72,7 @@ prefs = {
 }
 browser = Watir::Browser.new :chrome, options: { prefs: prefs }
 
+# TODO: Add Firefox support (maybe)
 #profile = Selenium::WebDriver::Firefox::Profile.new
 #profile['browser.download.folderList'] = 2 # custom location
 #profile['browser.download.dir'] = "/Fileserver/nas/video effects"
@@ -78,29 +108,47 @@ library_links = [library_links[LIB_NUMBER - 1]] if LIB_NUMBER
 library_links.each do |lib_link|
   puts 'Moving to /Downloads/[LIBRARY]'
   browser.goto lib_link.href
+
   # Wait for cards to load
   browser.elements(tag_name: 'p', class: 'Duration').wait_until(&:exists?)
-  #sleep 30
   items = browser.elements(tag_name: 'i', class: 'DownloadCloud')
-  # 10 minutes between iterations
-  sleep_time_total = (60 * 10)
-  sleep_time = sleep_time_total
+
+  # Time between iterations
+  seconds_to_sleep = (60 * MINUTES)
+  sleep_delta = seconds_to_sleep
+
   items.each_with_index do |item, index|
-    # ensure we respect the 5 per 5 minute lamesauce
-    next if FIRST_ITEM && index < (FIRST_ITEM - 1)
-    if index > 0 && index % 3 == 0 && index != (FIRST_ITEM - 1)
-      sleep sleep_time if sleep_time > 0
-      sleep_time = sleep_time_total
+    # Ensure we respect the 5 per 5 minute lamesauce
+    next if START && index < (START - 1)
+    if index > 0 && index % 3 == 0 && index != (START - 1)
+      # Reached the end of the loop, so we
+      # sleep any time remaining then continue...
+      sleep sleep_delta if sleep_delta > 0
+      sleep_delta = seconds_to_sleep
     end
+
+    # Download the thing
     title = item.parent.parent.
       elements(class: 'ContentExtraInfoSuperTitle').first.text
+    puts "Downloading #{title}"
+    item.click
+
+    # Dynamically determine sleep time between
+    # downloads at static rate of 1MB/sec
     file_size_value = item.parent.parent.
       elements(class: 'ContentExtraInfoSubTitle').last.text
     file_size = file_size_value.include?('GB') ?
       (file_size_value.to_f * 1000).to_i : file_size_value.to_i
-    puts "Downloading #{title}"
-    item.click
-    sleep_time = (sleep_time - file_size)
+    file_size =
+      case options['speed']
+      when 'fast'
+        (file_size / 2).to_i
+      when 'slow'
+        (file_size * 2).to_i
+      else
+        file_size
+      end
+    sleep_delta = (sleep_delta - file_size)
     sleep file_size
   end
 end
